7e08b5f504a910e65759bf6a7682274c1af72982
Debashish Mishra <debaonline@gmail.com>
2014-12-12 15:36:29 +0530

    Debashish | #00 | Adding API Test to Create Data in MCI

--- src/test/java/data/PatientData.java
+++ src/test/java/data/PatientData.java
@@ -2,6 +2,8 @@ package data;
 
 import domain.Address;
 import domain.Patient;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PatientData {
 
@@ -34,9 +36,6 @@ public class PatientData {
             .build();
 
     private  String id1 = String.valueOf(System.currentTimeMillis()).substring(7);
-//    private  String id1 = "435316";
-    private  String id3 = String.valueOf(System.currentTimeMillis() + 1).substring(7);
-
 
     public  Patient defaultPatient = new Patient.PatientBuilder()
             .firstName("A" + id1).lastName("ATEST")
@@ -64,15 +63,127 @@ public class PatientData {
             .occupation("Business")
             .primaryContact("Primary One").build();
 
+    private  String id4 = String.valueOf(System.currentTimeMillis() + 1).substring(3);
     public  Patient patientWithAllFieldDetails = new Patient.PatientBuilder()
-            .firstName("A" + id3).lastName("ATEST")
+            .firstName("A" + id4).lastName("ATEST")
             .gender("Male").dateOfBirth("01-03-2000")
             .address(defaultAddress)
             .education("5th Pass and Below")
             .occupation("Business")
             .primaryContact("Primary One")
-            .binBRN("90000000000" + id3).nid("9000000" + id3).uid("90000" + id3)
-            .fatherBRN("91000000000" + id3).fatherNid("9100000" + id3).fatherUid("91000" + id3)
-            .motherBRN("92000000000" + id3).motherNid("9200000" + id3).motherUid("92000" + id3)
+            .binBRN("8000000" + id4).nid("800" + id4).uid("8" + id4)
+            .fatherBRN("8100000" + id4).fatherNid("810" + id4).fatherUid("8" + id4)
+            .motherBRN("8200000" + id4).motherNid("820" + id4).motherUid("8" + id4)
             .build();
+
+    public JSONObject getAddressJsonForBarisal() {
+        JSONObject present_address = new JSONObject();
+        try {
+            present_address.put("address_line", "Test");
+            present_address.put("division_id", "10");
+            present_address.put("district_id", "09");
+            present_address.put("upazilla_id", "18");
+            present_address.put("city_corporation_id", "16");
+            present_address.put("ward_id", "01");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return present_address;
+    }
+
+    public JSONObject getAddressJsonForDhaka() {
+        JSONObject present_address = new JSONObject();
+        try {
+            present_address.put("address_line", "Test");
+            present_address.put("division_id", "30");
+            present_address.put("district_id", "26");
+            present_address.put("upazilla_id", "18");
+            present_address.put("city_corporation_id", "99");
+            present_address.put("ward_id", "42");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return present_address;
+    }
+    public JSONObject getAddressJsonForKhulna() {
+        JSONObject present_address = new JSONObject();
+        try {
+            present_address.put("address_line", "Test");
+            present_address.put("division_id", "40");
+            present_address.put("district_id", "47");
+            present_address.put("upazilla_id", "53");
+            present_address.put("city_corporation_id", "99");
+            present_address.put("ward_id", "10");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return present_address;
+    }
+    public JSONObject getAddressJsonForChittagong() {
+        JSONObject present_address = new JSONObject();
+        try {
+            present_address.put("address_line", "Test");
+            present_address.put("division_id", "20");
+            present_address.put("district_id", "19");
+            present_address.put("upazilla_id", "81");
+            present_address.put("city_corporation_id", "99");
+            present_address.put("ward_id", "72");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return present_address;
+    }
+
+    public JSONObject getAddressJsonForRajshahi() {
+        JSONObject present_address = new JSONObject();
+        try {
+            present_address.put("address_line", "Test");
+            present_address.put("division_id", "50");
+            present_address.put("district_id", "70");
+            present_address.put("upazilla_id", "66");
+            present_address.put("city_corporation_id", "99");
+            present_address.put("ward_id", "89");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return present_address;
+    }
+
+    public JSONObject getAddressJsonForRangpur() {
+        JSONObject present_address = new JSONObject();
+        try {
+            present_address.put("address_line", "Test");
+            present_address.put("division_id", "55");
+            present_address.put("district_id", "32");
+            present_address.put("upazilla_id", "30");
+            present_address.put("city_corporation_id", "33");
+            present_address.put("ward_id", "01");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return present_address;
+    }
+
+    public JSONObject getAddressJsonForSylhet() {
+        JSONObject present_address = new JSONObject();
+        try {
+            present_address.put("address_line", "Test");
+            present_address.put("division_id", "55");
+            present_address.put("district_id", "32");
+            present_address.put("upazilla_id", "30");
+            present_address.put("city_corporation_id", "33");
+            present_address.put("ward_id", "01");
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return present_address;
+    }
+
 }
---
+++ src/test/java/tests/datasetup/MCIBulkDataCreation.java
@@ -0,0 +1,209 @@
+package tests.datasetup;
+
+
+import categories.ApiTest;
+import com.jayway.restassured.RestAssured;
+import com.jayway.restassured.config.RestAssuredConfig;
+import data.PatientData;
+import domain.Patient;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+import utils.WebDriverProperties;
+
+import static com.jayway.restassured.RestAssured.basic;
+import static com.jayway.restassured.RestAssured.given;
+import static com.jayway.restassured.config.EncoderConfig.encoderConfig;
+
+
+public class MCIBulkDataCreation {
+
+    protected Patient primaryPatient;
+
+    @Before
+    public void setUp() {
+
+        RestAssured.baseURI = "http://172.18.46.56";
+        RestAssured.baseURI = WebDriverProperties.getProperty("mciURL");
+        RestAssured.port = 8081;
+        RestAssured.basePath = "/api/v1";
+        RestAssured.authentication = basic("mci", "password");
+        RestAssured.rootPath = "";
+        RestAssured.config = new RestAssuredConfig().encoderConfig(encoderConfig().defaultContentCharset("UTF-8"));
+    }
+
+
+    @Category(ApiTest.class)
+    @Test
+    public void createMultiplePatient() {
+
+        long startTime = System.currentTimeMillis();
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " ");
+            verifyCreatePatientForBarisalDivision();
+            verifyCreatePatientForDhakaDivision();
+            verifyCreatePatientForKhulnaDivision();
+            verifyCreatePatientForChittagongDivision();
+            verifyCreatePatientForRajshahiDivision();
+            verifyCreatePatientForRangpurDivision();
+            verifyCreatePatientForSylhetDivision();
+        }
+
+        long endTime = System.currentTimeMillis();
+        System.out.println("Time Taken: " + (endTime - startTime) / 1000 + " Seconds");
+    }
+
+
+    public void verifyCreatePatientForBarisalDivision() {
+
+        PatientData dataStore = new PatientData();
+        primaryPatient = dataStore.patientWithAllFieldDetails;
+
+        JSONObject person = createPatientDataToPost(dataStore);
+        try {
+            person.put("present_address", dataStore.getAddressJsonForBarisal());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+
+        createPatient(person);
+    }
+
+    public void verifyCreatePatientForDhakaDivision() {
+
+        PatientData dataStore = new PatientData();
+        primaryPatient = dataStore.patientWithAllFieldDetails;
+
+        JSONObject person = createPatientDataToPost(dataStore);
+        try {
+            person.put("present_address", dataStore.getAddressJsonForDhaka());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        createPatient(person);
+    }
+
+    public void verifyCreatePatientForKhulnaDivision() {
+
+        PatientData dataStore = new PatientData();
+        primaryPatient = dataStore.patientWithAllFieldDetails;
+
+        JSONObject person = createPatientDataToPost(dataStore);
+        try {
+            person.put("present_address", dataStore.getAddressJsonForKhulna());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        createPatient(person);
+    }
+
+    public void verifyCreatePatientForChittagongDivision() {
+
+        PatientData dataStore = new PatientData();
+        primaryPatient = dataStore.patientWithAllFieldDetails;
+
+        JSONObject person = createPatientDataToPost(dataStore);
+        try {
+            person.put("present_address", dataStore.getAddressJsonForChittagong());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        createPatient(person);
+    }
+
+    public void verifyCreatePatientForRangpurDivision() {
+
+        PatientData dataStore = new PatientData();
+        primaryPatient = dataStore.patientWithAllFieldDetails;
+
+        JSONObject person = createPatientDataToPost(dataStore);
+        try {
+            person.put("present_address", dataStore.getAddressJsonForRangpur());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        createPatient(person);
+    }
+
+    public void verifyCreatePatientForRajshahiDivision() {
+
+        PatientData dataStore = new PatientData();
+        primaryPatient = dataStore.patientWithAllFieldDetails;
+
+        JSONObject person = createPatientDataToPost(dataStore);
+        try {
+            person.put("present_address", dataStore.getAddressJsonForRajshahi());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        createPatient(person);
+    }
+
+    public void verifyCreatePatientForSylhetDivision() {
+
+        PatientData dataStore = new PatientData();
+        primaryPatient = dataStore.patientWithAllFieldDetails;
+
+        JSONObject person = createPatientDataToPost(dataStore);
+        try {
+            person.put("present_address", dataStore.getAddressJsonForSylhet());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        createPatient(person);
+    }
+
+    private void createPatient(JSONObject person) {
+//                System.out.println(person.toString());
+        given().contentType("application/json")
+                .body(person.toString())
+                .when().post("/patients")
+                .then().assertThat().statusCode(201);
+
+//        given().pathParam("nid", primaryPatient.getNid())
+//                .when().get("/patients?nid={nid}")
+//                .then()
+//                .body("results.hid[0]", Matchers.notNullValue());
+        System.out.println("Patient with NID " + primaryPatient.getNid() + " Created in MCI");
+    }
+
+    private JSONObject createPatientDataToPost(PatientData dataStore) {
+        JSONObject person = new JSONObject();
+
+        try {
+
+            JSONObject phone_number = new JSONObject();
+            person.put("nid", primaryPatient.getNid());
+            person.put("bin_brn", primaryPatient.getBinBRN());
+            person.put("uid", primaryPatient.getUid());
+            person.put("given_name", primaryPatient.getFirstName());
+            person.put("sur_name", primaryPatient.getLastName());
+            person.put("date_of_birth", "2000-03-01");
+            person.put("gender", "M");
+            person.put("occupation", "02");
+            person.put("edu_level", "02");
+
+            phone_number.put("country_code", "88");
+            phone_number.put("area_code", "");
+            phone_number.put("number", "01678904560");
+            phone_number.put("extension", "");
+            person.put("phone_number", phone_number);
+
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return person;
+    }
+
+    @After
+    public void tearDown() {
+        RestAssured.reset();
+    }
+
+}
