2ecf8d1f029b6f8e7f30bb7b6007927608d14eef
ashutosh <ashutoks@thoughtworks.com>
2015-04-03 17:22:54 +0530

    Ashutosh|deleting unwanted files

--- shr_api_test/spec/mci_mciAdmin.spec.js
+++
@@ -1,7 +0,0 @@
-var request = require('request');
-var User = require('../lib/user');
-
-var SSORequest = require('../lib/SSORequest');
-var Patient = require('../lib/patient');
-var PatientRequest = require('../lib/patientRequest');
-var CatchmentRequest = require('../lib/CatchmentRequest');
