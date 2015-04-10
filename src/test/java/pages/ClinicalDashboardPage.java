7bafc5e8c1cd54db4f575d024f7a4b51a8b57d1b
Debashish Mishra <debaonline@gmail.com>
2015-04-10 12:23:14 +0530

    BSHR-000 | Debashish | Temp Fix for Vitals Sync test (adding units to asserts)

--- src/test/java/pages/ClinicalDashboardPage.java
+++ src/test/java/pages/ClinicalDashboardPage.java
@@ -80,10 +80,10 @@ public class ClinicalDashboardPage extends Page {
             }
 
         }
-        Assert.assertEquals("Vital Verification Temperature ", patientVitals.getTemperature(), vitals.get("Temperature"));
-        Assert.assertEquals("Vital Verification Pulse ", patientVitals.getPulse(), vitals.get("Pulse"));
-        Assert.assertEquals("Vital Verification Diastolic BP ", patientVitals.getDiastolicBloodPressure(), vitals.get("Diastolic"));
-        Assert.assertEquals("Vital Verification Systolic BP ", patientVitals.getSystolicBloodPressure(), vitals.get("Systolic"));
+        Assert.assertEquals("Vital Verification Temperature ", patientVitals.getTemperature()+" F", vitals.get("Temperature"));
+        Assert.assertEquals("Vital Verification Pulse ", patientVitals.getPulse()+" /min", vitals.get("Pulse"));
+        Assert.assertEquals("Vital Verification Diastolic BP ", patientVitals.getDiastolicBloodPressure()+" mm Hg", vitals.get("Diastolic"));
+        Assert.assertEquals("Vital Verification Systolic BP ", patientVitals.getSystolicBloodPressure()+" mm Hg", vitals.get("Systolic"));
         System.out.println("Vitals Data verified for the Patient ");
     }
 }
