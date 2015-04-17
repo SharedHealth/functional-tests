5883ebb5d9842a85e2db3212ea44159c5aa34eb3
Rajeshvaran Appasamy <a.rajeshvaran@gmail.com>
2015-04-17 22:27:00 +0530

    BSHR-000 | Rajeshvaran | Fixing the household code issue

--- shr_api_test/src/entity/patient.js
+++ shr_api_test/src/entity/patient.js
@@ -1,7 +1,7 @@
 module.exports = function(confidentiality) {
  	var nid = new Date().getTime();
 	var bin_brn =  nid + "0000";
-	var houseHoldCode=Math.ceil(Math.random()*100000000);
+	var houseHoldCode=Math.random().toString().substring(2,11);
 	var name=Math.ceil(Math.random()*1000000)
 
 	return {
